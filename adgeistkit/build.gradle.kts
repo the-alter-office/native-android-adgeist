56d6c41ac93043402ef63c34c421af9ef96ff190
kishore <kishore@leaguex.com>
2025-05-02 14:51:03 +0530

    fix: updated jitpack yml file

--- adgeistkit/build.gradle.kts
+++ adgeistkit/build.gradle.kts
@@ -52,10 +52,9 @@ afterEvaluate {
         publications {
             create<MavenPublication>("release") {
                 from(components["release"])
-
                 groupId = "com.github.the-alter-office"
                 artifactId = "adgeistkit"
-                version = "0.0.1"
+                version = "0.0.2"
             }
         }
     }
