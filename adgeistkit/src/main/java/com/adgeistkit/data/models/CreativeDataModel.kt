c7d56abc9a450d229eb5cbd8fc50cd2a88768069
Joynal Dsouza <joynal@thealteroffice.com>
2026-06-30 15:51:52 +0530

    fix : deprecate type of old cpm response

--- adgeistkit/src/main/java/com/adgeistkit/data/models/CreativeDataModel.kt
+++ adgeistkit/src/main/java/com/adgeistkit/data/models/CreativeDataModel.kt
@@ -4,38 +4,6 @@ import com.google.gson.annotations.SerializedName
 
 sealed interface AdResponseData
 
-data class CPMAdResponse(
-    val success: Boolean,
-    val message: String,
-    val data: BidResponseData?
-) : AdResponseData
-
-data class BidResponseData(
-    val id: String,
-    val seatBid: List<SeatBid>,
-    val bidId: String,
-    val cur: String
-)
-
-data class SeatBid(
-    val bidId: String,
-    val bid: List<Bid>
-)
-
-data class Bid(
-    val id: String,
-    val impId: String,
-    val price: Double,
-    val ext: BidExtension
-)
-
-data class BidExtension(
-    val creativeUrl: String,
-    val ctaUrl: String,
-    val creativeTitle: String,
-    val creativeDescription: String,
-)
-
 data class FixedAdResponse(
     val isTest: Boolean?,
     val expiresAt: String?,
